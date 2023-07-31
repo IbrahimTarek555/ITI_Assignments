128f505920fb9c19f4a34efc72ed98d41376109c
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-07-31 09:22:37 +0300

    Add Lecture 3 Labs.

---
+++ C/Lecture_3/Labs/Lab1.c
@@ -0,0 +1,19 @@
+/*Include needed libraries*/
+#include "stdio.h"
+
+/*It's the main*/
+int main()
+{
+	/*Print a name 10 times*/
+	for(int i = 0; i < 10; i++)
+	{
+		printf("Ibrahim");
+		
+		if(i != 9)
+		{
+			printf("\n");
+		}
+	}
+	/*Return to the operating system*/
+	return 0;
+}
