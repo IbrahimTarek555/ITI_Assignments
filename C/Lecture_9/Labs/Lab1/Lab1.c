82cfc7b5dd3a76ac760523b3b34e65bdc2943bff
Ibrahim Tarek <ebrahimtarek555@gmail.com>
2023-07-31 09:25:45 +0300

    Add Lecture 9 Labs.

---
+++ C/Lecture_9/Labs/Lab1/Lab1.c
@@ -0,0 +1,12 @@
+/*Needed libraries*/
+#include <stdio.h>
+
+/*It is the main*/
+int main()
+{
+	/*Print my name to the user*/
+	printf("My name is Ibrahim.");
+	
+	/*Return to the operating system*/
+	return 0;
+}
